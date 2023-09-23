e799bdb9b42c5da2ebc2b0700109a44c3e336dea
ljleb <lebel.louisjacob@gmail.com>
2023-09-23 02:42:39 -0400

    infotext

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,4 @@
-from modules import scripts
+from modules import scripts, processing
 import gradio as gr
 from lib_free_u import global_state, unet, xyz_grid
 
@@ -17,7 +17,7 @@ class FreeUScript(scripts.Script):
                     label="Enable",
                     value=False,
                 )
-                set_def = gr.Button(
+                reset_to_defaults = gr.Button(
                         value="Reset to Defaults",
                         size="sm",
                     )
@@ -48,20 +48,53 @@ class FreeUScript(scripts.Script):
                     value=0.2,
                 )
 
-        def reset_to_defaults():
-            return 1.2, 0.9, 1.4, 0.2
+        reset_to_defaults.click(
+            fn=lambda: (1.2, 0.9, 1.4, 0.2),
+            outputs=[b0, s0, b1, s1],
+        )
 
-        set_def.click(reset_to_defaults, outputs=[b0, s0, b1, s1])
+        backbone_infotext = gr.HTML(visible=False, interactive=False)
+        skip_infotext = gr.HTML(visible=False, interactive=False)
+
+        backbone_infotext.change(
+            fn=self.on_update_scales,
+            inputs=[backbone_infotext],
+            outputs=[backbone_infotext, b0, b1],
+        )
+        skip_infotext.change(
+            fn=self.on_update_scales,
+            inputs=[skip_infotext],
+            outputs=[skip_infotext, b0, b1],
+        )
+
+        self.infotext_fields = [
+            (backbone_infotext, "FreeU Backbone Scales"),
+            (skip_infotext, "FreeU Skip Scales"),
+        ]
+        self.paste_field_names = [p[1] for p in self.infotext_fields]
 
         return enabled, b0, s0, b1, s1
 
-    def process(self, p, enabled: bool, b0: float, s0: float, b1: float, s1: float):
+    def on_update_scales(self, infotext):
+        if not infotext:
+            return (gr.skip(),) * 3
+
+        return (
+            gr.update(value=""),
+            *(gr.update(value=float(b)) for b in infotext.split(",")),
+        )
+
+    def process(self, p: processing.StableDiffusionProcessing, enabled: bool, b0: float, s0: float, b1: float, s1: float):
         global_state.update(
             enabled=enabled,
             backbone_factors=[b0, b1],
             skip_factors=[s0, s1],
         )
         global_state.xyz_locked_attrs.clear()
+
+        if global_state.enabled:
+            p.extra_generation_params["FreeU Backbone Scales"] = ",".join(str(b) for b in global_state.backbone_factors)
+            p.extra_generation_params["FreeU Skip Scales"] = ",".join(str(b) for b in global_state.skip_factors)
 
 
 unet.patch_model()
