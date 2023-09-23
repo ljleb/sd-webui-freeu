cd2993aaf7d9e19fe00da9cc006e5c8926cee878
ljleb <lebel.louisjacob@gmail.com>
2023-09-22 21:11:38 -0400

    sdxl

--- lib_free_u/unet.py
+++ lib_free_u/unet.py
@@ -1,5 +1,6 @@
 from lib_free_u import global_state
 from ldm.modules.diffusionmodules import openaimodel
+from sgm.modules.diffusionmodules import openaimodel as openaimodel_sdxl
 from modules.sd_hijack_unet import th as torch
 
 
@@ -26,8 +27,9 @@ def Fourier_filter(x, threshold, scale):
 
 
 class UNetModel(openaimodel.UNetModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """
+    copied from repositories.stable-diffusion-stability-ai.ldm.modules.diffusionmodules.openaimodel
+    """
 
     def forward(self, x, timesteps=None, context=None, y=None, **kwargs):
         """
@@ -39,7 +41,7 @@ class UNetModel(openaimodel.UNetModel):
         :return: an [N x C x ...] Tensor of outputs.
         """
         if not global_state.enabled:
-            return OriginalUnetModel.forward(self, x, timesteps, context, y, **kwargs)
+            return OriginalUNetModel.forward(self, x, timesteps, context, y, **kwargs)
 
         assert (y is not None) == (
                 self.num_classes is not None
@@ -79,8 +81,66 @@ class UNetModel(openaimodel.UNetModel):
             return self.out(h)
 
 
-OriginalUnetModel = openaimodel.UNetModel
+class SDXLUNetModel(openaimodel_sdxl.UNetModel):
+    """
+    copied from repositories.generative-models.sgm.modules.diffusionmodules.openaimodel
+    """
+
+    def forward(self, x, timesteps=None, context=None, y=None, **kwargs):
+        """
+        Apply the model to an input batch.
+        :param x: an [N x C x ...] Tensor of inputs.
+        :param timesteps: a 1-D batch of timesteps.
+        :param context: conditioning plugged in via crossattn
+        :param y: an [N] Tensor of labels, if class-conditional.
+        :return: an [N x C x ...] Tensor of outputs.
+        """
+        if not global_state.enabled:
+            return OriginalSDXLUNetModel.forward(self, x, timesteps, context, y, **kwargs)
+
+        assert (y is not None) == (
+            self.num_classes is not None
+        ), "must specify y if and only if the model is class-conditional"
+        hs = []
+        t_emb = openaimodel_sdxl.timestep_embedding(timesteps, self.model_channels, repeat_only=False)
+        emb = self.time_embed(t_emb)
+
+        if self.num_classes is not None:
+            assert y.shape[0] == x.shape[0]
+            emb = emb + self.label_emb(y)
+
+        # h = x.type(self.dtype)
+        h = x
+        for module in self.input_blocks:
+            h = module(h, emb, context)
+            hs.append(h)
+        h = self.middle_block(h, emb, context)
+        for module in self.output_blocks:
+            hs_ = hs.pop()
+
+            # --------------- FreeU code -----------------------
+            # Only operate on the first two stages
+            if h.shape[1] == 1280:
+                h[:, :640] = h[:, :640] * global_state.backbone_factors[0]
+                hs_ = Fourier_filter(hs_, threshold=1, scale=global_state.skip_factors[0])
+            if h.shape[1] == 640:
+                h[:, :320] = h[:, :320] * global_state.backbone_factors[1]
+                hs_ = Fourier_filter(hs_, threshold=1, scale=global_state.skip_factors[1])
+            # ---------------------------------------------------------
+
+            h = torch.cat([h, hs_], dim=1)
+            h = module(h, emb, context)
+        h = h.type(x.dtype)
+        if self.predict_codebook_ids:
+            assert False, "not supported anymore. what the f*** are you doing?"
+        else:
+            return self.out(h)
+
+
+OriginalUNetModel = openaimodel.UNetModel
+OriginalSDXLUNetModel = openaimodel_sdxl.UNetModel
 
 
 def patch_model():
     openaimodel.UNetModel = UNetModel
+    openaimodel_sdxl.UNetModel = SDXLUNetModel
---
+++ lib_free_u/xyz_grid.py
@@ -0,0 +1,66 @@
+import functools
+from types import ModuleType
+from typing import Optional
+from modules import scripts, processing
+from lib_free_u import global_state
+
+
+def patch():
+    xyz_module = find_xyz_module()
+    xyz_module.axis_options.extend([
+        xyz_module.AxisOption("[Free U] Enabled", str_to_bool, apply_global_state("enabled"), choices=choices_bool),
+        xyz_module.AxisOption("[Free U] Block 1 Backbone Scale", float, apply_global_state("b1")),
+        xyz_module.AxisOption("[Free U] Block 1 Skip Scale", float, apply_global_state("s1")),
+        xyz_module.AxisOption("[Free U] Block 2 Backbone Scale", float, apply_global_state("b2")),
+        xyz_module.AxisOption("[Free U] Block 2 Skip Scale", float, apply_global_state("s2")),
+    ])
+    processing.process_images_inner = functools.partial(processing_process_images_inner_hijack, original_function=processing.process_images_inner)
+
+
+def processing_process_images_inner_hijack(*args, original_function, **kwargs):
+    global_state.xyz_locked_attrs.clear()
+    return original_function(*args, **kwargs)
+
+
+def apply_global_state(attr):
+    def callback(_p, v, _vs):
+        if hasattr(global_state, attr):
+            setattr(global_state, attr, v)
+            global_state.xyz_locked_attrs.add(attr)
+            return
+
+        index = int(attr[1:])
+        if "b" in attr:
+            factors = global_state.backbone_factors
+            global_state.xyz_locked_attrs.add("backbone_factors")
+        else:
+            factors = global_state.skip_factors
+            global_state.xyz_locked_attrs.add("skip_factors")
+
+        factors[index] = v
+
+    return callback
+
+
+def str_to_bool(string):
+    string = str(string)
+    if string in ["None", ""]:
+        return None
+    elif string.lower() in ["true", "1"]:
+        return True
+    elif string.lower() in ["false", "0"]:
+        return False
+    else:
+        raise ValueError(f"Could not convert string to boolean: {string}")
+
+
+def choices_bool():
+    return ["False", "True"]
+
+
+def find_xyz_module() -> Optional[ModuleType]:
+    for data in scripts.scripts_data:
+        if data.script_class.__module__ in {"xyz_grid.py", "xy_grid.py"} and hasattr(data, "module"):
+            return data.module
+
+    return None
