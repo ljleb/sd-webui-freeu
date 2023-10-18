498c38a3557192b9b109528107b7abe23bf454a0
ljleb <lebel.louisjacob@gmail.com>
2023-10-18 17:24:16 -0400

    v2

--- lib_free_u/xyz_grid.py
+++ lib_free_u/xyz_grid.py
@@ -10,9 +10,9 @@ def patch():
     if xyz_module is None:
         print("[sd-webui-freeu]", "xyz_grid.py not found.", file=sys.stderr)
         return
-
     xyz_module.axis_options.extend([
         xyz_module.AxisOption("[FreeU] Enabled", str_to_bool, apply_global_state("enable"), choices=choices_bool),
+        xyz_module.AxisOption("[FreeU] Version", str, apply_global_state("version", key_map=global_state.all_versions), choices=choices_version),
         xyz_module.AxisOption("[FreeU] Preset", str, apply_global_state("preset"), choices=choices_preset),
         xyz_module.AxisOption("[FreeU] Start At Step", int_or_float, apply_global_state("start_ratio")),
         xyz_module.AxisOption("[FreeU] Stop At Step", int_or_float, apply_global_state("stop_ratio")),
@@ -32,8 +32,10 @@ def patch():
     ])
 
 
-def apply_global_state(k):
+def apply_global_state(k, key_map=None):
     def callback(_p, v, _vs):
+        if key_map is not None:
+            v = key_map[v]
         global_state.xyz_attrs[k] = v
 
     return callback
@@ -60,6 +62,10 @@ def int_or_float(string):
 
 def choices_bool():
     return ["False", "True"]
+
+
+def choices_version():
+    return list(global_state.all_versions.keys())
 
 
 def choices_preset():
